34251c9bdf416b34fe8f30bd8030864ffb34ed27
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2025-02-22 00:04:37 +0700

    Delete Bai6_ADC directory

--- Bai6_ADC/Objects/kalman.d
+++
@@ -1,4 +0,0 @@
-.\objects\kalman.o: Kalman\kalman.c
-.\objects\kalman.o: Kalman\kalman.h
-.\objects\kalman.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\kalman.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
