c4497730d4264ea152b45faa88e76d01571afd42
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2025-04-12 00:36:57 +0700

    Delete Bai7_DMA_PWM directory

--- Bai7_DMA_PWM/Master/Objects/kalman.d
+++
@@ -1,4 +0,0 @@
-./objects/kalman.o: ..\..\..\HALA_ACADEMY\kalman.c \
-  ..\..\..\HALA_ACADEMY\kalman.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
