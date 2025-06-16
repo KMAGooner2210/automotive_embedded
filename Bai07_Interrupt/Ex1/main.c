57a8466f6216848dd6d4dc179f7083efa347c956
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2025-06-16 16:20:58 +0700

    Add files via upload

---
+++ Bai07_Interrupt/Ex1/main.c
@@ -0,0 +1,54 @@
+#include "stm32f10x.h"                  // Device header
+#include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
+#include "stm32f10x_exti.h"             // Keil::Device:StdPeriph Drivers:EXTI
+#include "stm32f10x_tim.h"              // Keil::Device:StdPeriph Drivers:TIM
+
+void RCC_Config(void){
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2ENR_AFIOEN, ENABLE);
+}
+void GPIO_Config(void){
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+}
+
+void EXTI_Config(void){
+	
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB,GPIO_PinSource0);
+	
+	EXTI_InitTypeDef EXTI_InitStructure;
+	EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+	EXTI_Init(&EXTI_InitStructure);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+void EXTI0_IRQHandler(void){
+	if(EXTI_GetITStatus(EXTI_Line0) != RESET){
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOA,GPIO_Pin_0)));
+		EXTI_ClearITPendingBit(EXTI_Line0);
+	}
+}
+
+int main(void){
+	RCC_Config();
+	GPIO_Config();
+	EXTI_Config();
+	while(1){
+	}
+}
